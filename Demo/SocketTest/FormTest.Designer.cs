8d8fd4c8c768e1fc99a884068e5d5cbfe0da77a2
GISwilson <942535021@qq.com>
2015-05-08 09:06:20 +0800

    添加界面与Excel导出功能

---
+++ Demo/SocketTest/FormTest.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace SocketTest
+{
+    partial class FormTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tabControl1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(583, 343);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP地址：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "4502";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "端口：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 47);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "服务器配置";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(583, 343);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.numericUpDown2);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(575, 317);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "大数据量测试";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(575, 317);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "传递字符串长度：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "测试连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(395, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "开始测试";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(538, 228);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(476, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "保存为表格";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "执行次数：";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(284, 19);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 21);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(109, 19);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            -1593835520,
+            466537709,
+            54210,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(89, 21);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // FormTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 409);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormTest";
+            this.Text = "测试";
+            this.Load += new System.EventHandler(this.FormTest_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Demo/SocketTest/FormTest.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SocketTest
+{
+    public partial class FormTest : Form
+    {
+        public FormTest()
+        {
+            InitializeComponent();
+        }
+
+        private MyServer myServer;
+        private DataTable dt;
+
+        
+
+        /// <summary>
+        /// 测试短时间高频发送数据
+        /// </summary>
+        void TestTime()
+        {
+            //myServer.ResetServerNum();
+            //System.Timers.Timer timer = new System.Timers.Timer();
+            //int count = 1000;
+            //timer.Interval = 1;
+            //int index = 1;
+            //string data = new string('a', 1000);
+            //timer.Elapsed += (sender, e) =>
+            //{
+            //    if (index > count)
+            //    {
+            //        timer.Stop();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(index.ToString());
+            //        myServer.SendStrData(index.ToString());
+            //        lock (data)
+            //        {
+            //            index++;
+            //            data += new string('a', index);
+            //        }
+            //    }
+            //};
+            //timer.Start();
+        }
+
+        /// <summary>
+        /// 测试长时间连接的可靠性
+        /// </summary>
+        void TestLongConnect()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000000;
+            timer.Elapsed += (sender, e) =>
+            {
+                myServer.SendStrData(DateTime.Now.ToLongTimeString());
+            };
+            timer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myServer = new MyServer();
+            var splitIps=textBox1.Text.Split('.');
+
+            System.Net.IPAddress ip = System.Net.IPAddress.Loopback;
+            System.Net.IPAddress.TryParse(textBox1.Text, out ip);
+            int port = 4502;
+            int.TryParse(textBox2.Text, out port);
+            var endPoint = new System.Net.IPEndPoint(ip, port);
+            var state = myServer.Connect(endPoint);
+            while (!state.IsCompleted)
+            {
+            }
+            MessageBox.Show(string.Format("连接{0}！", state.Result ? "成功" : "失败"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TestBigData(Convert.ToInt32(numericUpDown2.Value),Convert.ToInt32(numericUpDown1.Value));
+        }
+
+        /// <summary>
+        /// 测试数据量大的情形
+        /// </summary>
+        void TestBigData(int startValue, int times)
+        {
+            List<long> values = new List<long>();
+            for (int i = 0; i < times; i++)
+            {
+                Thread.Sleep(10);
+                string msg = new string('a', startValue);
+                //List<int> data = new List<int>(count);
+                //for (int j = 0; j < count; j++)
+                //{
+                //    data.Add(j);
+                //}
+                Console.WriteLine(i.ToString());
+                var watch = Stopwatch.StartNew();
+                watch.Start();
+                //var aa = myServer.GetItself(data);
+                var aa = myServer.GetItself(msg);
+                while (!aa.IsCompleted)
+                {
+                }
+                watch.Stop();
+                DataRow row = dt.NewRow();
+                row[0] = startValue;
+                row[1] = i;
+                row[2] = watch.ElapsedMilliseconds;
+                dt.Rows.Add(row);
+                values.Add(watch.ElapsedMilliseconds);
+                //var value=myServer.GetCount(data);
+                //Console.WriteLine("计算结果为：");
+                //Console.WriteLine(value.Result.ToString());
+            }
+            DataRow rowSum = dt.NewRow();
+            rowSum[0] = startValue;
+            rowSum[1] = "合计";
+            rowSum[2] = string.Format("均值{0}，方差{1}", values.GetAverage().ToString(), values.GetBiaozhunCha().ToString());
+            dt.Rows.Add(rowSum);
+
+            dataGridView1.DataSource = dt;
+            //for (int count = startValue; count < int.MaxValue; count *= times)
+            //{
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        Thread.Sleep(10);
+            //        string msg = new string('a', count);
+            //        //List<int> data = new List<int>(count);
+            //        //for (int j = 0; j < count; j++)
+            //        //{
+            //        //    data.Add(j);
+            //        //}
+            //        Console.WriteLine(i.ToString());
+            //        var watch = Stopwatch.StartNew();
+            //        watch.Start();
+            //        //var aa = myServer.GetItself(data);
+            //        var aa = myServer.GetItself(msg);
+            //        while (!aa.IsCompleted)
+            //        {
+            //            //Thread.Sleep(10);
+            //        }
+            //        watch.Stop();
+            //        Console.WriteLine(string.Format("结果为{0}，耗时：{1}ms", aa.Result.Length.ToString(), watch.ElapsedMilliseconds.ToString()));
+            //        times.Add(watch.ElapsedMilliseconds);
+            //        //var value=myServer.GetCount(data);
+            //        //Console.WriteLine("计算结果为：");
+            //        //Console.WriteLine(value.Result.ToString());
+            //    }
+            //    Console.WriteLine(string.Format("以上测试平均时间为{0}ms，方差为{1}ms", times.GetAverage(), times.GetBiaozhunCha()));
+            //}
+        }
+
+        private void FormTest_Load(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            dt.Columns.Add("字符串长度");
+            dt.Columns.Add("序号");
+            dt.Columns.Add("时间(毫秒)");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel文件|*.xls";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ExcelRender.ExcelRender.RenderToExcel(dt, dialog.FileName);
+            }
+        }
+    }
+
+    
+}
